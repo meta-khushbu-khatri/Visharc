ed7e61fa21a28ebd1a7bd15fdd539a49f55e4717
Khushbu Khatri <khushbukhatri2000@gmail.com>
2026-03-14 17:22:05 +0530

    fix github pages build

--- vite.config.js
+++ vite.config.js
@@ -3,9 +3,6 @@ import react from "@vitejs/plugin-react";
 import tailwindcss from "@tailwindcss/vite";
 
 export default defineConfig({
-base: "/Visharc-project/",   // GitHub Pages repository name
-plugins: [
-react(),
-tailwindcss(),
-],
+  base: "/Visharc/",
+  plugins: [react(), tailwindcss()],
 });
